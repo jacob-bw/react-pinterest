5df8bc7e38f36db6e7edd6ddb6532727a84d4c47
Jacob <wittenberg.jacob@gmail.com>
2019-12-10 21:47:28 -0600

    auth working

--- src/App/App.js
+++ src/App/App.js
@@ -1,27 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import firebase from 'firebase/app';
+import firebaseConnection from '../helpers/data/connection';
 import './App.scss';
 
-function App() {
-  return (
+import Auth from '../components/Auth/Auth';
+
+firebaseConnection.firebaseApp();
+
+class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  render() {
+    const { authed } = this.state;
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button className="btn btn-primary">React Pinterest</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="btn btn-primary">React Pinterest</button>
+    {
+      (authed) ? (<div>You Logged In</div>) : (<Auth />)
+    }
     </div>
-  );
+    );
+  }
 }
 
 export default App;
